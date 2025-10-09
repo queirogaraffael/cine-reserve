a3a48e8e83cdd3e2c1db4c7735046d930220f1bd
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2025-10-09 16:45:40 -0300

    perf: adicionar caching a api de Genre

--- cinema-API/src/main/java/com/example/cinema/api/domain/services/GenreService.java
+++ cinema-API/src/main/java/com/example/cinema/api/domain/services/GenreService.java
@@ -8,6 +8,8 @@ import com.example.cinema.api.shared.dtos.genre.GenreUpdateDTO;
 import com.example.cinema.api.shared.exceptions.GeneroJaExisteException;
 import com.example.cinema.api.shared.exceptions.ResourceNotFoundException;
 import com.example.cinema.api.shared.mappers.GenreMapper;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -28,12 +30,14 @@ public class GenreService {
     }
 
     @Transactional
+    @CachePut(value = "genres", key = "#result.id")
     public GenreResponseDTO create(GenreRequestDTO dto) {
         Genre genero = genreMapper.toEntity(dto);
         return genreMapper.toDTO(genreRepository.save(genero));
     }
 
     @Transactional(readOnly = true)
+    @Cacheable(value = "genres", key = "#id")
     public GenreResponseDTO findById(Long id) {
         Genre genre = genreRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Gênero não encontrado"));
@@ -53,6 +57,7 @@ public class GenreService {
     }
 
     @Transactional
+    @CachePut(value = "genres", key = "#id")
     public GenreResponseDTO update(Long id, GenreUpdateDTO dto) {
         Genre genre = genreRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Gênero não encontrado"));
--- cinema-API/src/main/java/com/example/cinema/api/resources/GenreResource.java
+++ cinema-API/src/main/java/com/example/cinema/api/resources/GenreResource.java
@@ -1,30 +1,38 @@
 package com.example.cinema.api.resources;
 
+import com.example.cinema.api.domain.services.GenreService;
 import com.example.cinema.api.shared.dtos.genre.GenreRequestDTO;
 import com.example.cinema.api.shared.dtos.genre.GenreResponseDTO;
 import com.example.cinema.api.shared.dtos.genre.GenreUpdateDTO;
-import com.example.cinema.api.domain.services.GenreService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
+import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-@Tag(name = "Gêneros")
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+@Tag(name = "Genres")
 @RestController
 @RequestMapping("/api/genres")
 @EnableMethodSecurity
 public class GenreResource {
 
     private final GenreService genreService;
+    private final CacheControl cacheControl;
 
-    public GenreResource(GenreService genreService) {
+    public GenreResource(GenreService genreService, @Value("${cache.ttl}") long cacheTtl) {
         this.genreService = genreService;
+        this.cacheControl = CacheControl.maxAge(cacheTtl, TimeUnit.SECONDS).cachePublic();
     }
 
     @Operation(summary = "Criar novo gênero", description = "Cria um novo gênero")
@@ -35,7 +43,12 @@ public class GenreResource {
     @SecurityRequirement(name = "Bearer Authentication")
     @PostMapping()
     public ResponseEntity<GenreResponseDTO> createGenre(@RequestBody @Valid GenreRequestDTO dto) {
-        return ResponseEntity.status(201).body(genreService.create(dto));
+
+        GenreResponseDTO responseDTO = genreService.create(dto);
+
+         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                 .buildAndExpand(responseDTO.getId()).toUri();
+         return ResponseEntity.created(uri).body(responseDTO);
     }
 
     @GetMapping("/{id}")
@@ -44,7 +57,7 @@ public class GenreResource {
     @ApiResponse(responseCode = "404", description = "Gênero não encontrado")
     @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
     public ResponseEntity<GenreResponseDTO> findById(@PathVariable Long id) {
-        return ResponseEntity.ok(genreService.findById(id));
+        return ResponseEntity.ok().cacheControl(cacheControl).body(genreService.findById(id));
     }
 
     @GetMapping()
@@ -53,7 +66,7 @@ public class GenreResource {
     @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
     public ResponseEntity<?> findAllPageable(@RequestParam(defaultValue = "0") int page,
                                              @RequestParam(defaultValue = "10") int size) {
-        return ResponseEntity.ok(genreService.findAllPageable(page, size));
+        return ResponseEntity.ok().cacheControl(CacheControl.noCache().cachePrivate()).body(genreService.findAllPageable(page, size));
     }
 
     @GetMapping("/search")
@@ -63,7 +76,7 @@ public class GenreResource {
     public ResponseEntity<Page<GenreResponseDTO>> findByNameContainingIgnoreCase(@RequestParam String name,
                                                                                  @RequestParam(defaultValue = "0") int page,
                                                                                  @RequestParam(defaultValue = "10") int size) {
-        return ResponseEntity.ok().body(genreService.findByNameContainingIgnoreCase(name, page, size));
+        return ResponseEntity.ok().cacheControl(CacheControl.noCache().cachePrivate()).body(genreService.findByNameContainingIgnoreCase(name, page, size));
     }
 
     @PreAuthorize("hasRole('ADMIN')")
