e320a8355e6031dd9c2380beec579aa4be54c965
Raffael Queiroga <raffaelqueirogaa@gmail.com>
2026-01-13 21:13:39 -0300

    refactor: melhoria no tratamento de erro do evento de criação de compra

--- cinema-API/src/main/java/com/example/cinema/api/domain/purchase/listeners/EmailPurchaseNotificationListener.java
+++ cinema-API/src/main/java/com/example/cinema/api/domain/purchase/listeners/EmailPurchaseNotificationListener.java
@@ -2,12 +2,14 @@ package com.example.cinema.api.domain.purchase.listeners;
 
 import com.example.cinema.api.domain.purchase.event.PurchaseCreatedEvent;
 import com.example.cinema.api.domain.services.EmailService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
+@Slf4j
 public class EmailPurchaseNotificationListener {
 
     private final EmailService emailServicePort;
@@ -19,7 +21,13 @@ public class EmailPurchaseNotificationListener {
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handlePurchase(PurchaseCreatedEvent event) {
-        emailServicePort.sendPurchaseNotificationEmail(event.getUser(), event.getPurchase());
+
+        try {
+            emailServicePort.sendPurchaseNotificationEmail(event.getUser(), event.getPurchase());
+        } catch (Exception e) {
+            log.error("Erro ao enviar email da compra {}", event.getPurchase().getId(), e);
+        }
+
     }
 
 }
